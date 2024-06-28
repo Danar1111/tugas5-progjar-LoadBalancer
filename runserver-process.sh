dc9a7efd3df846075f157f56f13dd23188817aa0
Danar <114123567+Danar1111@users.noreply.github.com>
2024-06-28 20:32:05 +0700

    Add files via upload

---
+++ runserver-process.sh
@@ -0,0 +1,6 @@
+#jalankan 5 server thread
+
+python3 server_process_pool_http.py 8002 &
+python3 server_process_pool_http.py 8003 &
+python3 server_process_pool_http.py 8004 &
+python3 server_process_pool_http.py 8005 &
